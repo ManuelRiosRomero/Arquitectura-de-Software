68389d3819e7fed2e62c080729cea164d4f51020
Daniel Castellanos <78168171+R4DRHunt@users.noreply.github.com>
2023-03-24 20:51:46 -0500

    FrontAndroiStudio

---
+++ TallerArquitecturaKotlin/app/src/main/java/com/example/tallerarquitecturakotlin/SimpleHttpClient.kt
@@ -0,0 +1,40 @@
+import android.util.Log
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
+
+class SimpleHttpClient(private val url: String) {
+
+    private val client = OkHttpClient()
+
+    fun sendJson(json: String): String {
+        val requestBody = json.toRequestBody("application/json".toMediaTypeOrNull())
+
+        Log.d("Loquesta", requestBody.toString())
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        var response: Response? = null
+
+        try {
+            response = client.newCall(request).execute()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        catch (e: Exception) {
+
+            e.printStackTrace()
+        }
+
+        if (response != null) {
+            return response.body?.string() ?: "Error con el Query"
+        }
+
+        return "Response sis null"
+    }
+}
